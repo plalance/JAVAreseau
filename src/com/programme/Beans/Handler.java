7433e076f38d6aefd042f00d43fdfe749de5ff78
plalance <lalance.paul@gmail.com>
2017-05-23 10:13:12 +0200

    AJout reponse server

--- src/com/programme/Beans/Handler.java
+++ src/com/programme/Beans/Handler.java
@@ -3,35 +3,41 @@ package com.programme.Beans;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Vector;
 
 /**
  * Created by Thibault on 12/04/2017.
  */
-public class Handler extends Thread{
+public class Handler extends Thread {
     private final Socket socket;
+    private PrintWriter reponse;
 
     public Handler(Socket socket) { //constructeur
         super();
-        this.socket=socket;
+        this.socket = socket;
     }
 
     public void run() {
         BufferedReader reader;
-            try {
-                reader = new BufferedReader(
-                        new InputStreamReader(socket.getInputStream()));
-                String line = "";
-                while ((line=reader.readLine()) != null){
-                    System.out.println(line);
-                }
-                socket.shutdownInput();
+        try {
+            reader = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+                reponse = new PrintWriter(this.socket.getOutputStream());
+                reponse.println("Vous avez envoyé :"+line);
+                reponse.flush();
             }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
+            socket.shutdownInput();
 
-            System.out.println("Connexion terminée !");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Connexion terminée !");
     }
 }
--- src/com/programme/Beans/Joueur.java
+++ src/com/programme/Beans/Joueur.java
@@ -1,6 +1,8 @@
 package com.programme.Beans;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -9,11 +11,12 @@ public class Joueur{
 
     private String message;
     private Socket socket;
+    private BufferedReader reponse;
 
     public void createSocket() {
 
         try {
-            socket = new Socket("192.168.1.47", 6969);
+            socket = new Socket("192.168.43.33", 6969);
             socket.setKeepAlive(true);
             // simulation d'attente
 //            socket.setKeepAlive(true);
@@ -49,6 +52,14 @@ public class Joueur{
             PrintWriter writer = new PrintWriter(this.getSocket().getOutputStream());
             writer.print(message+"\n");
             writer.flush();
+
+            reponse = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            String line = "";
+            while ((line = reponse.readLine()) != null) {
+                System.out.println(line);
+            }
+
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
